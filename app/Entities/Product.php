7b9f7da050e81476f2fb143f1aa768fa00b11fbf
Willem Leuverink <willem@mediacode.nl>
2018-10-16 09:45:45 +0200

    Implement the interface

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -3,8 +3,48 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use DeltaProc\ShoppingCart\Contracts\Purchasable;
 
-class Product extends Model
+class Product extends Model implements Purchasable
 {
-    //
+    /**
+     * Get the product display title
+     *
+     * @return String
+     */
+    public function getLineTitle() : String
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the product display description
+     *
+     * @return String
+     */
+    public function getLineDescription() : String
+    {
+        return $this->description;
+    }
+
+    /**
+     * Get the unique identifier for the product
+     *
+     *
+     * @return String
+     */
+    public function getLineIdentifier() : String
+    {
+        return (String) $this->id;
+    }
+
+    /**
+     * Get the price for this item
+     *
+     * @return Integer
+     */
+    public function getLinePrice() : Integer
+    {
+        $this->price;
+    }
 }
